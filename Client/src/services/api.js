f0612f7e9912c145cc34cb99add228e60823af47
Anam Parwez <anamparwez18@gmail.com>
2026-02-06 19:55:48 +0530

    Update api.js

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   // Vite auto-detect karega:
   // Local pe: khali rahega (vite proxy use hoga)
   // Vercel pe: VITE_API_URL wala link lega
-  baseURL: import.meta.env.VITE_API_URL || '', 
+  baseURL: import.meta.env.VITE_API_URL || 'https://royal-estate-uzii.onrender.com', 
   withCredentials: true,
 });
 
